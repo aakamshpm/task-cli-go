a02d0f1ad30f28d51de6b269b60bce139e91b943
Aakamsh <aakamshpm@gmail.com>
2026-04-13 17:14:06 +0530

    getting used to go structure

---
+++ internal/task/service.go
@@ -0,0 +1,63 @@
+package task
+
+import (
+	"errors"
+	"strings"
+	"sync"
+	"time"
+)
+
+var ErrEmptyTitle = errors.New("title cannot be empty")
+
+// custom time type named 'Clock'
+// this is handy in test cases where we would create a fixed time and pass it to instance of service during task creation or any other operation
+// we are using a type function becaues the field 'now' in 'Service' struct uses a function call to get current time.
+// so in real usage, we pass real current time. but in test, we create a fixed time and use this function to pass it.
+type Clock func() time.Time
+
+type Service struct {
+	mu      sync.RWMutex
+	tasks   []Task
+	nextInt int
+	now     Clock
+}
+
+// in GO, we don't have class, new keyword, and typical constructor creation and usage with "this" like other languages
+// hence we need to create a custom constuctor function starting with 'New' keyword for convention followed by the type name. Eg: NewService
+func NewService(now Clock) *Service {
+	if now == nil {
+		now = time.Now // we assign the Now() funtion to the field 'now', this will be called each time on task creation. NOTE: Service.now is a function
+	}
+
+	return &Service{
+		tasks:   make([]Task, 0), // we intentionaly create a slice thats empty and ready to grow
+		nextInt: 1,
+		now:     now,
+	}
+}
+
+// *Service ensures that Add function is under type Service and usage outside
+// the 's' here acts as a 'this' instance
+func (s *Service) Add(title string) (Task, error) {
+	title = strings.TrimSpace(title)
+
+	if title == "" {
+		return Task{}, ErrEmptyTitle
+	}
+
+	// solves reader-writer problem
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	t := Task{
+		ID:        s.nextInt,
+		Title:     title,
+		Done:      false,
+		CreatedAt: s.now().UTC(),
+	}
+
+	s.tasks = append(s.tasks, t)
+	s.nextInt++
+
+	return t, nil
+}
